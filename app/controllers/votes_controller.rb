3ba8412c03cefab4b654c180e36d6fe25cffca81
Takanao Yoshii <tyoshii716@yahoo.co.jp>
2011-11-03 16:10:33 +0900

    added get profile

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -14,41 +14,17 @@ class VotesController < ApplicationController
     else
 
       access_token = @facebook_cookies['access_token']
-      graph = Koala::Facebook::GraphAPI.new(access_token)
-      @friends = graph.get_object("me/friends")
+      @graph = Koala::Facebook::GraphAPI.new(access_token)
+      @profile = @graph.get_object("me")
+      @friends = @graph.get_object("me/friends")
 
       require 'pp'
       pp @friends
+      pp @profile
+
 
       render :votes
     end
   end
 
-  # GET /votes/test
-  def test
-
-    puts Facebook::APP_ID
-
-    if @facebook_cookies.nil?
-      render :index
-    else
-
-      url = 'https://graph.facebook.com/'
-      url << Facebook::APP_ID
-      url << '/accounts/test-users?installed=true&name=HogeFuga&'
-      url << 'permissions=publish_stream,email,offline_access,read_stream&'
-      url << 'method=post&'
-      url << Facebook::SECRET
-
-      test_user = open( url ){|t| 
-        ActiveSupport::JSON.decode(t.read) 
-      }
-      #=>{"id"=>"id", "login_url"=>"url", "access_token" =>"access_token"}      
-      
-
-
-      require 'pp'
-      pp test_user
-    end
-  end
 end
