19df7be597e540c4149ed75b464d7abeef4255f1
Takanao Yoshii <tyoshii716@yahoo.co.jp>
2011-11-05 13:04:50 +0900

    update spothon vote

--- app/assets/javascripts/spothon_vote.js
+++ app/assets/javascripts/spothon_vote.js
@@ -1,11 +1,45 @@
+
 $(function() {
-  function next {
-    right.each( function() {
 
-    );
-    right[0].
+  function decrement_q () {
+    var q = $("#left-question-num");
+    var now = q.text();
 
+    q.text( now - 1 );
+  }
+  
+  function next() {
+    var q = $("#left-question-num");
+    var id = q.text();
+
+    $(".q-word").css("display", "none");
+    $(".user").css("display", "none");
+  
+    $("#q-"    + id).css("display", "block");
+    $("#user-" + id).css("display", "block");
+    $(".sig").attr("style", "display:none");
   }
 
+
+  // mouse hover
+  var to_light = function() { $(this).css("background-color", "lightcyan") };
+  var to_white = function() { $(this).css("background-color", "white")     };
+
+  $(".right").hover( to_light, to_white );
+  $(".left").hover( to_light, to_white );
+
+  // click user
+  $(".right").click(
+    function() {
+      var sig = $(this).find(".sig").text();
+
+      // ajax post sig
+
+      decrement_q();
+      next();
+    }
+  );
+  
+  // init
   next();
 });
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,3 +1,6 @@
+require 'openssl'
+# -*- coding: utf-8 -*-
+
 class VotesController < ApplicationController
 
   before_filter :parse_facebook_cookies  
@@ -5,6 +8,26 @@ class VotesController < ApplicationController
   def parse_facebook_cookies
     @facebook_cookies ||= Koala::Facebook::OAuth.new.get_user_info_from_cookie(cookies)
   end
+
+  def encrypt( val )
+    enc = OpenSSL::Cipher::Cipher.new('aes256')
+    enc.encrypt
+    enc.pkcs5_keyivgen( 'spothon' )
+    (enc.update(val) + enc.final).unpack("H*")[0]
+    rescue
+      false
+  end
+
+  def decrypt( val )
+    v = Array.new
+    v << val
+    dec = OpenSSL::Cipher::Cipher.new('aes256') 
+    dec.decrypt 
+    dec.pkcs5_keyivgen( 'spothon' )
+    dec.update( v.pack("H*")) + dec.final
+    rescue  
+      false 
+  end 
 
   # GET /votes
   def index
@@ -33,16 +56,27 @@ class VotesController < ApplicationController
           'right' => {
             'name'  => f1['name'],
             'img'   => 'http://graph.facebook.com/' << f1['id'] << '/picture',
-          }
+            'sig'   => encrypt( f1['id'].to_s + Time.current.to_i.to_s ) 
+          },
           'left' => {
             'name'  => f2['name'],
             'img'   => 'http://graph.facebook.com/' << f2['id'] << '/picture',
+            'sig'   => encrypt( f2['id'].to_s + Time.current.to_i.to_s )
           }
         }
 
         loop_count -= 1
       end
-
+      
+      # question
+      @question = Array.new
+=begin
+      @question << { 'id' => 1, 'q' => "野球が好きなのはどっち？"       }
+      @question << { 'id' => 2, 'q' => "サッカーが好きなのはどっち？"   }
+      @question << { 'id' => 3, 'q' => "ラグビーが好きなのはどっち？"   }
+      @question << { 'id' => 4, 'q' => "ゴルフが好きなのはどっち？"     }
+      @question << { 'id' => 5, 'q' => "フットサルが好きなのはどっち？" }
+=end
       render :votes
     end
   end
